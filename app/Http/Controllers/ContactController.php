d4c0b7ce4340ae200583dfaaa821559ded6ff26d
Steve Creekmore <Creekmore108@gmail.com>
2021-07-13 18:19:50 -0600

    fixed redirect back to #contact if validation fails

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,25 +4,37 @@ namespace App\Http\Controllers;
 
 use RealRashid\SweetAlert\Facades\Alert;
 use App\Models\Contact;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class ContactController extends Controller
 {
-    public function store(){
 
-        request()->validate([
-            'first_name' => 'required|gt:3',
+    public function store(Request $request){
+        
+
+        $data = Validator::make($request->all(),  [
+            'first_name' => 'required',
             'last_name' => '',
-            'email' => 'email|required',
+            'email' => 'email|required', 
             'organization' => '',
             'phone' => '',
             'comment' => 'required',
             'referral' => '',
         ]);
-        // dd(request());
-        Contact::create(request()->all());
+        
+        if ($data->fails()) {
+            return Redirect::to(URL::previous() . "#contact")->withErrors($data)->withInput();
+        }
+
+        Contact::create($request->all());
         
         Alert::success('success','Thank you, we will be in contact with you');
-        return redirect()->route('home');
+        
+        return Redirect::to(URL::previous() . "#contact");
     }
 
 
--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -22,27 +22,27 @@
               <div>
                 <label for="first_name" class="block text-sm font-medium text-gray-700">First Name</label>
                 <div class="mt-1">
-                  <input type="text" name="first_name" id="first_name"   placeholder="First name" class="block w-full shadow-sm sm:text-sm focus:ring-yellow-600  focus:border-yellow-600 border-gray-300 rounded-md">
+                  <input type="text" name="first_name" id="first_name"  value="{{ old('first_name') }}" placeholder="First name" class="block w-full shadow-sm sm:text-sm focus:ring-yellow-600  focus:border-yellow-600 border-gray-300 rounded-md">
                   @error('first_name') <span class="text-red-600">{{ $message }}</span> @enderror
                 </div>
               </div>
               <div>
                 <label for="last_name" class="block text-sm font-medium text-gray-700">Last Name</label>
                 <div class="mt-1">
-                  <input type="text" name="last_name" id="last_name" placeholder="Last name" class="block w-full shadow-sm sm:text-sm focus:ring-yellow-600 focus:border-yellow-600 border-gray-300 rounded-md">
+                  <input type="text" name="last_name" id="last_name" value="{{ old('last_name') }}" placeholder="Last name" class="block w-full shadow-sm sm:text-sm focus:ring-yellow-600 focus:border-yellow-600 border-gray-300 rounded-md">
                 </div>
               </div>
               <div class="sm:col-span-2">
                 <label for="email" class="block text-sm font-medium text-gray-700">Email Address</label>
                 <div class="mt-1">
-                  <input id="email" name="email" type="email"  autocomplete="email"  placeholder="Email address" class="block w-full shadow-sm sm:text-sm focus:ring-yellow-600 focus:border-yellow-600 border-gray-300 rounded-md">
+                  <input id="email" name="email" type="email"  value="{{ old('email') }}" autocomplete="email"  placeholder="Email address" class="block w-full shadow-sm sm:text-sm focus:ring-yellow-600 focus:border-yellow-600 border-gray-300 rounded-md">
                   @error('email') <span class="text-red-600">{{ $message }}</span> @enderror
                 </div>
               </div>
               <div class="sm:col-span-2">
                 <label for="organization" class="block text-sm font-medium text-gray-700">Organization</label>
                 <div class="mt-1">
-                  <input type="text" name="organization" id="organization"  placeholder="Organization name" autocomplete="organization" class="block w-full shadow-sm sm:text-sm  focus:ring-yellow-600 focus:border-yellow-600 border-gray-300 rounded-md">
+                  <input type="text" name="organization" id="organization" value="{{ old('organization') }}" placeholder="Organization name" autocomplete="organization" class="block w-full shadow-sm sm:text-sm  focus:ring-yellow-600 focus:border-yellow-600 border-gray-300 rounded-md">
                 </div>
               </div>
               <div class="sm:col-span-2">
@@ -51,7 +51,7 @@
                   <span id="phone" class="text-sm text-gray-500">Optional</span>
                 </div>
                 <div class="mt-1">
-                  <input type="text" name="phone" id="phone" autocomplete="tel" placeholder="Phone number"  class="block w-full shadow-sm focus:ring-yellow-600 focus:border-yellow-600 border-gray-300 rounded-md">
+                  <input type="text" name="phone" id="phone" autocomplete="tel"  value="{{ old('phone') }}" placeholder="Phone number"  class="block w-full shadow-sm focus:ring-yellow-600 focus:border-yellow-600 border-gray-300 rounded-md">
                 </div>
               </div>
               <div class="sm:col-span-2">
@@ -60,15 +60,15 @@
                   <span id="comment" class="text-sm text-gray-500">Max. 500 characters</span>
                 </div>
                 <div class="mt-1">
-                  <textarea id="comment" name="comment"  rows="4" class="block w-full shadow-sm sm:text-sm focus:ring-yellow-600 focus:border-yellow-600  border border-gray-300 rounded-md"></textarea>
-                  @error('content') <span class="text-red-600">{{ $message }}</span> @enderror
+                  <textarea id="comment" name="comment"  rows="4" class="block w-full shadow-sm sm:text-sm focus:ring-yellow-600 focus:border-yellow-600  border border-gray-300 rounded-md">{{ old('comment') }}</textarea>
+                  @error('comment') <span class="text-red-600">{{ $message }}</span> @enderror
                 </div>
               </div>
               
               <div class="sm:col-span-2">
                 <label for="referral" class="block text-sm font-medium text-gray-700">How did you hear about us?</label>
                 <div class="mt-1">
-                  <input type="text" name="referral" id="referral" class="shadow-sm focus:ring-yellow-600 focus:border-yellow-600  block w-full sm:text-sm border-gray-300 rounded-md">
+                  <input type="text" name="referral" id="referral" value="{{ old('referral') }}" class="shadow-sm focus:ring-yellow-600 focus:border-yellow-600  block w-full sm:text-sm border-gray-300 rounded-md">
                 </div>
               </div>
               <div class="text-center sm:col-span-2">
