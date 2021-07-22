7e37bd56b6806b7fa491e14180b10c14eaabcca3
Steve Creekmore <Creekmore108@gmail.com>
2021-07-21 18:56:08 -0600

    moved sweet alert javascript back to the base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -46,6 +46,54 @@
 
         @livewireScripts
         
-        <!-- <script src="js/index.js"></script> -->
+        <script>
+                     
+    // one is located in header.blade.php menu item "Live Demo" 
+document.querySelector(".one").addEventListener('click', function(){ 
+Swal.fire({title: 'We are still in development',text: 'subscribe to our email list to be notified when we are in production',confirmButtonColor: '#D97706',
+        showClass: {popup: 'animate__animated animate__fadeInDown'},hideClass: {popup: 'animate__animated animate__fadeOutUp'}})})
+
+    // two and three are in the hero.blade.php buttons
+document.querySelector(".two").addEventListener('click', function(){ 
+Swal.fire({title: 'We are still in development',text: 'subscribe to our email list to be notified when we are in production',confirmButtonColor: '#D97706',
+        showClass: {popup: 'animate__animated animate__fadeInDown'},hideClass: {popup: 'animate__animated animate__fadeOutUp'}})})
+
+document.querySelector(".three").addEventListener('click', function(){ 
+Swal.fire({title: 'We are still in development',text: 'subscribe to our email list to be notified when we are in production',confirmButtonColor: '#D97706',
+        showClass: {popup: 'animate__animated animate__fadeInDown'},hideClass: {popup: 'animate__animated animate__fadeOutUp'}})})
+
+    // four and five are in alternating_features.blade.php
+document.querySelector(".four").addEventListener('click', function(){ 
+Swal.fire({title: 'We are still in development',text: 'subscribe to our email list to be notified when we are in production',confirmButtonColor: '#D97706',
+        showClass: {popup: 'animate__animated animate__fadeInDown'},hideClass: {popup: 'animate__animated animate__fadeOutUp'}})})
+
+document.querySelector(".five").addEventListener('click', function(){ 
+Swal.fire({title: 'We are still in development',text: 'subscribe to our email list to be notified when we are in production',confirmButtonColor: '#D97706',
+        showClass: {popup: 'animate__animated animate__fadeInDown'},hideClass: {popup: 'animate__animated animate__fadeOutUp'}})})
+
+    // six is in pricing.blade.php
+document.querySelector(".six").addEventListener('click', function(){ 
+Swal.fire({title: 'We are still in development',text: 'subscribe to our email list to be notified when we are in production',confirmButtonColor: '#D97706',
+        showClass: {popup: 'animate__animated animate__fadeInDown'},hideClass: {popup: 'animate__animated animate__fadeOutUp'}})})
+    
+    // seven is in ctc.blade.php 
+document.querySelector(".seven").addEventListener('click', function(){ 
+Swal.fire({title: 'We are still in development',text: 'subscribe to our email list to be notified when we are in production',confirmButtonColor: '#D97706',
+        showClass: {popup: 'animate__animated animate__fadeInDown'},hideClass: {popup: 'animate__animated animate__fadeOutUp'}})})
+
+    // eight is in footer.blade.php
+document.querySelector(".eight").addEventListener('click', function(){ 
+Swal.fire({title: 'We are still in development',text: 'subscribe to our email list to be notified when we are in production',confirmButtonColor: '#D97706',
+        showClass: {popup: 'animate__animated animate__fadeInDown'},hideClass: {popup: 'animate__animated animate__fadeOutUp'}})})
+
+    // nine is in the header.blade.php mobile menu "live Demo"
+document.querySelector(".nine").addEventListener('click', function(){ 
+Swal.fire({title: 'We are still in development',text: 'subscribe to our email list to be notified when we are in production',confirmButtonColor: '#D97706',
+        showClass: {popup: 'animate__animated animate__fadeInDown'},hideClass: {popup: 'animate__animated animate__fadeOutUp'}})})
+                
+       
+
+
+        </script>
     </body>
 </html>
