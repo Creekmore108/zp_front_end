3e51e2f75cba3a56b79b660627e8cb636998b2a4
Steve Creekmore <Creekmore108@gmail.com>
2021-08-02 05:58:26 -0600

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\EmailListController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
@@ -29,26 +19,22 @@ Route::view('/jobs', 'jobs')->name('jobs');
 Route::view('/press', 'press')->name('press');
 Route::view('/terms', 'terms')->name('terms');
 Route::view('/privacy', 'privacy')->name('privacy');
-
 Route::post('/emailList', [EmailListController::class, 'store'])->name('emailList');
 Route::post('/contact', [ContactController::class, 'store'])->name('contacts');
-
-Route::get('/emails', [EmailListController::class, 'show'])->name('emails');
-Route::get('/contacts', [ContactController::class, 'show'])->name('contacts');
-
+ 
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)
         ->name('login');
-
+    
     Route::get('register', Register::class)
         ->name('register');
- });
+        
+    Route::get('password/reset', Email::class)
+        ->name('password.request');
 
-Route::get('password/reset', Email::class)
-    ->name('password.request');
-
-Route::get('password/reset/{token}', Reset::class)
-    ->name('password.reset');
+    Route::get('password/reset/{token}', Reset::class)
+        ->name('password.reset');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('email/verify', Verify::class)
@@ -57,13 +43,14 @@ Route::middleware('auth')->group(function () {
 
     Route::get('password/confirm', Confirm::class)
         ->name('password.confirm');
-});
 
-Route::middleware('auth')->group(function () {
     Route::get('email/verify/{id}/{hash}', EmailVerificationController::class)
         ->middleware('signed')
         ->name('verification.verify');
 
     Route::post('logout', LogoutController::class)
         ->name('logout');
+
+    Route::get('/emails', [EmailListController::class, 'show'])->name('emails');
+    Route::get('/contacts', [ContactController::class, 'show'])->name('contacts');
 });
