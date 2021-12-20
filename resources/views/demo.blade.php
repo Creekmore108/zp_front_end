5b191b5cce9339bb53811aeb9021499c5f6de477
Steve Creekmore <Creekmore108@gmail.com>
2021-12-19 21:00:57 -0700

    new event added to demo

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -16,7 +16,9 @@
                         </button>
                         <div>
                             <x-icon.logo class="h-10 w-auto mt-5 mr-5"/>
+
                         </div>
+                        
                     </div>
                 <div class="mt-3">
                     <nav class="mt-3 space-y-1 flex-1 px-2  bg-gray-600">
@@ -99,6 +101,7 @@
                             <x-icon.logo class="h-20 w-auto"></x-icon.logo>
                         </div>
                     </a>
+                    <div class="ml-20"><a href="/new">New Event</a></div>
                     <!-- Sidebar component, swap this element with another sidebar if you like -->
                     <nav class="mt-3 space-y-1 flex-1 px-2  bg-gray-600">
                         <a href="#" class="one hover:no-underline group flex items-center px-2 py-2 text-sm leading-5 font-medium  rounded-md text-black bg-gradient-to-r from-yellow-600 to-yellow-800 hover:text-white hover:from-yellow-800 hover:to-yellow-600 focus:outline-none  focus:ring-1 focus:ring-yellow-800 focus:border-yellow-600 focus:outline-none focus:border-yellow-800 focus:ring ring-yellow-800 disabled:opacity-25 transition ease-in-out duration-150">
